b02db7357394e99c24b9725adf32b34802695f31
John Hook <jhook1@ufl.edu>
2020-12-03 16:26:30 -0500

    Added lstm model

--- modelanalysis.py
+++ modelanalysis.py
@@ -7,6 +7,10 @@ from sklearn.metrics import mean_squared_error, r2_score
 import matplotlib.pyplot as plt
 from collections import OrderedDict
 from joblib import dump, load 
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
+import math
 
 def genPlotModel(ticker):
     df = pd.read_csv('data/%s.csv'%ticker, sep = ',', header = 0)
@@ -66,6 +70,84 @@ def genPlotModel(ticker):
 
     plt.savefig('figs/%s.png'%ticker)
     plt.close()
+    
+    ################################### - LSTM - ##################################################
+
+    #Create a new Dataframe
+    data = df['Close']
+    # print(type(data))
+    #Convert to numpy array
+    dataset = np.array(data.values)
+    dataset = np.reshape(dataset, (-1, 1))
+    #print(dataset)
+    #Get the number of rows to train the model
+    training_data_len = math.ceil(len(dataset) * .8)
+
+    #Scale the data
+    scaler = MinMaxScaler(feature_range=(0,1))
+    scaled_data = scaler.fit_transform(dataset)
+    #print(scaled_data)
+
+    #Create the scaled training data set
+    train_data = scaled_data[0:training_data_len, :]
+    #Split the data into x_train and y_train
+    x_train = []
+    y_train = []
+    for i in range(n, len(train_data)):
+        x_train.append(train_data[i-n:i, 0])
+        y_train.append(train_data[i, 0])
+    
+    #Convert the x_train and y_train to numpy arrays
+    x_train, y_train = np.array(x_train), np.array(y_train)
+
+    #Reshape the data
+    # (#samples, timesteps, and features)
+    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+    x_train.shape
+
+    #Build the LSTM model
+    model = Sequential()
+    # 50 neurons, (timesteps, features)
+    model.add(LSTM(50, return_sequences=True, input_shape = (x_train.shape[1], 1)))
+    model.add(LSTM(50, return_sequences=False))
+    model.add(Dense(25))
+    model.add(Dense(1))
+
+    model.compile(optimizer='adam', loss ='mean_squared_error')
+
+    model.fit(x_train, y_train, batch_size = 1, epochs=1)
+
+    #Create the testing dataset
+    #Create a new array containing scaled values from index size-n to size
+    # [last n values, all the columns]
+    test_data = scaled_data[training_data_len - n:, :]
+    #Create the datasets x_test, y_test
+    x_test = []
+    # 61st values
+    y_test = dataset[training_data_len:, :]
+    for i in range(n, len(test_data)):
+        # Past n values
+        x_test.append(test_data[i-n:i, 0])
+
+    #convert to numpy array
+    x_test = np.array(x_test)
+    # print(x_test.shape)
+
+    #Reshape the data for the LSTM model to 3-D
+    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+    #Get the models predicted price and values
+    predictions = model.predict(x_test)
+    # We want predictions to contain the same values as y_test dataset
+    predictions = scaler.inverse_transform(predictions)
+    # print(type(predictions))
+
+    #Get the root mean squred error (RMSE) - lower the better
+    rmse = np.sqrt(np.mean(predictions - y_test)**2)
+    print("LSTM RMSE for %s: "%ticker, rmse)
+
+    ################################### - LSTM - ##################################################
+
 
     return lr_confidence
 
