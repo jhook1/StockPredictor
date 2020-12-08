801f62d1a9d88ffd1f40f60e8e8c993873f6d626
Abhinay Pasunuri <apasunuri@ufl.edu>
2020-12-07 20:26:01 -0500

    Added models

--- modelanalysis.py
+++ modelanalysis.py
@@ -1,30 +1,23 @@
 import numpy as np
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+import pandas_datareader
 from sklearn.linear_model import LinearRegression
 from sklearn.svm import SVR
 from sklearn.model_selection import train_test_split
-import pandas as pd
 from sklearn.metrics import mean_squared_error, r2_score
-import matplotlib.pyplot as plt
 from collections import OrderedDict
 from joblib import dump, load 
 from sklearn.preprocessing import MinMaxScaler
-from keras.models import Sequential
+from keras.models import Sequential, load_model
 from keras.layers import Dense, LSTM
 import math
 
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
 def genLRPlotModel(ticker):
     df = pd.read_csv('data/%s.csv'%ticker, sep = ',', header = 0)
-
-    df.shape
-    """
-    plt.figure(figsize=(16,8))
-    plt.title('Close Price History')
-    plt.plot(df['Close'])
-    plt.xlabel('Date', fontsize=18)
-    plt.ylabel('Close Price USD ($)', fontsize=18)
-    #plt.show()
-    plt.close()
-    """
 
     X = np.array(df['Close'])
 
@@ -52,8 +45,8 @@ def genLRPlotModel(ticker):
 
     lr_prediction = lr.predict(x_test1)
     train = x_train1
-    valid = df.drop(['Date', 'Prediction'],1)[-n:]
-    valid['Predictions'] = lr_prediction
+    actual = df.drop(['Date', 'Prediction'], 1)[-n:]
+    actual['Predictions'] = lr_prediction
 
     df['lr_prediction']=df['Prediction']
     df['lr_prediction'][-n:]=lr_prediction
@@ -64,7 +57,7 @@ def genLRPlotModel(ticker):
     plt.xlabel('Date', fontsize=18)
     plt.ylabel('Close Price USD ($)', fontsize=18)
     plt.plot(df['Close'])
-    plt.plot(valid[['Close', 'Predictions']])
+    plt.plot(actual[['Close', 'Predictions']])
     plt.legend(['Train', 'Val', 'Predictions'])
     #plt.show()
 
@@ -73,22 +66,23 @@ def genLRPlotModel(ticker):
 
     return lr_confidence
     
-    ################################### - LSTM - ##################################################
+################################### - LSTM - ##################################################
 def trainLSTM(ticker):
     #Create a new Dataframe
-    data = df['Close']
-    # print(type(data))
+    df = pandas_datareader.DataReader('V', data_source = 'yahoo', start = '2015-01-01', end = '2020-10-01')
+    data = df[['Close']]
+    n = 60
+
     #Convert to numpy array
     dataset = np.array(data.values)
     dataset = np.reshape(dataset, (-1, 1))
-    #print(dataset)
+    
     #Get the number of rows to train the model
     training_data_len = math.ceil(len(dataset) * .8)
 
     #Scale the data
     scaler = MinMaxScaler(feature_range=(0,1))
     scaled_data = scaler.fit_transform(dataset)
-    #print(scaled_data)
 
     #Create the scaled training data set
     train_data = scaled_data[0:training_data_len, :]
@@ -105,7 +99,6 @@ def trainLSTM(ticker):
     #Reshape the data
     # (#samples, timesteps, and features)
     x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
-    x_train.shape
 
     #Build the LSTM model
     model = Sequential()
@@ -118,15 +111,15 @@ def trainLSTM(ticker):
 
     model.compile(optimizer='adam', loss ='mean_squared_error')
 
-    model.fit(x_train, y_train, batch_size = 1, epochs = 4)
+    model.fit(x_train, y_train, batch_size = 1, epochs = 5)
 
     #Create the testing dataset
     #Create a new array containing scaled values from index size-n to size
     # [last n values, all the columns]
     test_data = scaled_data[training_data_len - n:, :]
+
     #Create the datasets x_test, y_test
     x_test = []
-    # 61st values
     y_test = dataset[training_data_len:, :]
     for i in range(n, len(test_data)):
         # Past n values
@@ -134,7 +127,6 @@ def trainLSTM(ticker):
 
     #convert to numpy array
     x_test = np.array(x_test)
-    # print(x_test.shape)
 
     #Reshape the data for the LSTM model to 3-D
     x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
@@ -143,16 +135,84 @@ def trainLSTM(ticker):
     predictions = model.predict(x_test)
     # We want predictions to contain the same values as y_test dataset
     predictions = scaler.inverse_transform(predictions)
-    # print(type(predictions))
+
+    file_name = 'lstm_models/' + ticker + '.h5'
+    model.save(file_name)
 
     #Get the root mean squred error (RMSE) - lower the better
-    rmse = np.sqrt(np.mean(predictions - y_test)**2)
+    rmse = np.sqrt(np.mean(predictions - y_test) ** 2)
     print("LSTM RMSE for %s: "%ticker, rmse)
 
     ################################### - LSTM - ##################################################
 
 def plotLSTM(ticker):
-    pass
+    #Create a new Dataframe
+    n = 60
+    df = pandas_datareader.DataReader(ticker, data_source = 'yahoo', start = '2015-01-01', end = '2020-10-01')
+    data = df[['Close']]
+
+    dataset = np.array(data.values)
+    dataset = np.reshape(dataset, (-1, 1))
+
+    #Get the number of rows to train the model
+    training_data_len = math.ceil(len(data) * .8)
+
+    scaler = MinMaxScaler(feature_range=(0,1))
+    scaled_data = scaler.fit_transform(dataset)
+
+    #Create the scaled training data set
+    train_data = scaled_data[0:training_data_len, 0:4]
+    #Split the data into x_train and y_train
+    x_train = []
+    y_train = []
+    for i in range(n, len(train_data)):
+        x_train.append(train_data[i-n:i, 0:4])
+        y_train.append(train_data[i, :])
+
+    x_train, y_train = np.array(x_train), np.array(y_train)
+
+    test_data = scaled_data[training_data_len - n:, :]
+    #Create the datasets x_test, y_test
+    x_test = []
+    # 61st values
+    y_test = dataset[training_data_len:, :]
+    for i in range(n, len(test_data)):
+        # Past n values
+        x_test.append(test_data[i-n:i, 0])
+    x_test = np.array(x_test)
+    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+    file_name = 'lstm_models/' + ticker + '.h5'
+    model = load_model(file_name)
+    predictions = model.predict(x_test)
+    predictions = scaler.inverse_transform(predictions)
+
+    train = data[:training_data_len]
+    valid = data[training_data_len:]
+    #train = data.loc[:'2020-09-01']
+    #valid = data.loc['2020-09-01':]
+    #print(data.loc[:'2020-09-01'])
+    valid['Predictions'] = predictions
+    #print(type(train))
+    #print(type(valid))
+    #predictions_series = []
+    #indices = list(range(162, 202))
+    #for prediction in predictions:
+    #    predictions_series.append(prediction)
+    #predictions = pd.Series(predictions_series, index = indices)
+
+    # Visulaize the date
+    plt.figure(figsize=(16,8))
+    plt.title(ticker.upper())
+    plt.xlabel('Date', fontsize=12)
+    plt.ylabel('Closing Price USD ($)', fontsize=12)
+    plt.plot(train['Close'])
+    plt.plot(valid['Close'])
+    plt.plot(valid['Predictions'])
+    plt.legend(['Train', 'Actual', 'Predictions'], loc='lower right')
+    plt.savefig('lstm_figs/' + ticker + '.png')
+    #plt.show()
+
 
 if __name__ == '__main__':
     dow_jones_dict = OrderedDict()
@@ -190,11 +250,14 @@ if __name__ == '__main__':
 
     confTot = 0
 
-    for stock in list(dow_jones_dict.keys()):
-        confCurr = genLRPlotModel(stock)
-        trainLSTM(stock)
-        confTot += confCurr
+    plotLSTM('aapl')
+    plotLSTM('msft')
+    plotLSTM('v')
+    #for stock in list(dow_jones_dict.keys()):
+    #    confCurr = genLRPlotModel(stock)
+    #    trainLSTM(stock)
+    #    confTot += confCurr
     
-    confAvg = confTot / 31
+    #confAvg = confTot / 31
 
-    print("Average lr confidence is: ", confAvg)
+    #print("Average lr confidence is: ", confAvg)
